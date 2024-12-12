09725fa73a584a5554dfb7878748efffc0c004be
Guilherme <guilhermebarroso166@gmail.com>
2024-12-12 16:59:27 -0300

    texto do sobre-mim criado

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -2,8 +2,14 @@ import styles from './AboutMe.module.css'
 export default function AboutMe(){
     return(
         <section id="aboutMe" className={styles.AboutMe}>
-            <h1>Sobre Mim</h1>
-            <p>olá</p>
+            <h1 className={styles.title}>Sobre Mim</h1>
+            <div>
+                <p className={styles.text}>Sou Guilherme Barroso, estudante do 5º semestre de Ciência da Computação e desenvolvedor web. 
+                    Tenho 19 anos e, apesar de ainda não ter experiência profissional, venho adquirindo conhecimento e 
+                    desenvolvendo minhas habilidades através de projetos pessoais. Estou me dedicando à criação de sites funcionais
+                    e bem estruturados, sempre buscando aprender e me aperfeiçoar nas tecnologias mais modernas do mercado.
+                </p>
+            </div>
         </section>
     )
 }
