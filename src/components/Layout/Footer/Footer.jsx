b35d965bffcf2da7850cb26864c966ea0580d937
Guilherme <guilhermebarroso166@gmail.com>
2024-12-16 22:15:34 -0300

    footer estilizado e criação do arquivo vercel

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,9 +1,17 @@
 import styles from './Footer.module.css'
+import { FiInstagram } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
 import { useState } from 'react'
 export default function Footer(){
     const [email,setEmail] = useState('')
     const [message,setMessage] = useState('')
     const [status,setStatus] = useState('')
+    const [charAmout,setCharAmout] = useState('')
+
+    const handleChange = (e)=>{
+        setCharAmout(e.target.value.length)
+    }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -32,29 +40,47 @@ export default function Footer(){
         };
     return(
         <footer className={styles.footer}>
-            <div>
+            <div className={styles.emailContainer}>
                 <h1>Entre em contato comigo</h1>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor="email">Digite seu Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        id="email" 
-                        value={email}
-                        onChange={(e)=>setEmail(e.target.value)}
-                        required
-                    />
-                    <textarea
-                        id='message'
-                        value={message}
-                        onChange={(e)=>setMessage(e.target.value)}
-                    />
+                    <div className={styles.email}>
+                        <label htmlFor="email">Digite seu Email:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className={`${styles.input} ${styles.inputEmail}`}
+                            value={email}
+                            onChange={(e)=>setEmail(e.target.value)}
+                            required
+                            autoComplete='off'
+                        />
+                    </div>
+                    <div className={styles.message}>
+                        <textarea
+                            id='message'
+                            className={`${styles.input} ${styles.inputMessage}`}
+                            maxlength="250"
+                            value={message}
+                            onChange={(e)=>{
+                                setMessage(e.target.value)
+                                handleChange(e)
+                            }}
+                        
+                        />
+                        <span>{charAmout}/250</span>
+                    </div>
                     <button type='submit'>Enviar</button>
                 </form>
                 <p>{status}</p>
             </div>
-            <div>
-                <h1>Redes Sociais</h1>
+            <div className={styles.socialMedia}>
+                <h1>Mídias Sociais</h1>
+                <div>
+                <FiInstagram className={`${styles.icons} ${styles.insta}`}/>
+                <CiLinkedin className={`${styles.icons} ${styles.linkedin}`}/>
+                <FaGithub className={`${styles.icons} ${styles.github}`}/>
+                </div>
             </div>
         </footer>
     )
